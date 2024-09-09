02724a518348d713942ae703289f21f1cf6a5c8b
kfmx <tweem@hotmail.co.uk>
2024-09-09 23:33:57 +0200

    Added carousel autoplay. Renamed carousel component to Home.tsx. Added buttons and text to carousel slides. Added homepage text. Added missing "/" to pathnames. Added copyright text at bottom of page. Made navbar not shift content when opened. Made sure everything looks nice on mobile. Fixed tab indexes to improve screen reader readability.

--- src/App.tsx
+++ src/App.tsx
@@ -3,38 +3,50 @@ import '@mantine/core/styles.css';
 import '@mantine/carousel/styles.css';
 import styles from './Styles.module.css';
 import { Route, Routes, useLocation } from "react-router-dom";
-import { AppShell, Burger, Button, Group, Text } from '@mantine/core';
+import { AppShell, Burger, Button, Center, Group, Stack, Text } from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
 import HomePage from "./pages/Home";
-import CarouselPage from "./pages/Carousel";
 import ModeSelector from './components/ModeSelector';
 import ThemeSelector from './components/ThemeSelector';
 import { IconBrowser, IconCode, IconDeviceGamepad2, IconHome, IconUserCircle } from '@tabler/icons-react';
 import { Link } from 'react-router-dom';
 
 const navLinks: {pathname: string, label: string, icon: JSX.Element}[] = [
-  {pathname: "home", label: "Home", icon: <IconHome/>},
-  {pathname: "gamedesign", label: "Game Design", icon: <IconDeviceGamepad2/>},
-  {pathname: "projectleading", label: "Project Leading", icon: <IconUserCircle/>},
-  {pathname: "webprogramming", label: "Web Programming", icon: <IconBrowser/>},
-  {pathname: "gameprogramming", label: "Game Programming", icon: <IconCode/>}
+  {pathname: "/home", label: "Home", icon: <IconHome/>},
+  {pathname: "/gamedesign", label: "Game Design", icon: <IconDeviceGamepad2/>},
+  {pathname: "/projectleading", label: "Project Leading", icon: <IconUserCircle/>},
+  {pathname: "/webprogramming", label: "Web Programming", icon: <IconBrowser/>},
+  {pathname: "/gameprogramming", label: "Game Programming", icon: <IconCode/>}
 ]
 
-function App() {
+export default function App() {
   const [opened, { toggle }] = useDisclosure();
   const { pathname } = useLocation();
+
+  const navbarItems = navLinks.map(
+    navLink => (
+      <Link key={navLink.pathname} to={navLink.pathname} tabIndex={-1} style={pathname.includes(navLink.pathname) ? {borderColor: "var(--mantine-primary-color-9)"} : {}}>
+        <Button fullWidth variant="subtle" leftSection={navLink.icon} justify="start" tabIndex={opened ? 0 : -1}>
+          <Text c="var(--mantine-color-text)">
+            {navLink.label}
+          </Text>
+        </Button>
+      </Link>
+    )
+  )
 
   return (
     <AppShell
       header={{ height: 60 }}
       navbar={{
         width: 250,
-        breakpoint: 'sm',
+        breakpoint: "sm",
         collapsed: { mobile: !opened, desktop: !opened },
       }}
       padding="md"
       className={styles.appShell}
     >
+
       <AppShell.Header>
         <Group h="100%" px="md">
           <Burger
@@ -48,27 +60,27 @@ function App() {
           <ModeSelector/>
         </Group>
       </AppShell.Header>
+
       <AppShell.Navbar p="md" className={styles.navbar}>
-        {navLinks.map(
-          (navLink, i) => (
-            <Link key={i} to={navLink.pathname} style={pathname.includes(navLink.pathname) ? {borderColor: "var(--mantine-primary-color-9)"} : {}}>
-              <Button fullWidth variant="subtle" leftSection={navLink.icon} justify="start">
-                <Text c="var(--mantine-color-text)">
-                  {navLink.label}
-                </Text>
-              </Button>
-            </Link>
-          )
-        )}
+        {navbarItems}
       </AppShell.Navbar>
-      <AppShell.Main>
-        <Routes>
-          <Route path='/home' element={<CarouselPage/>}/>
-          <Route path='/*' element={<HomePage/>}/>
-        </Routes>
+
+      <AppShell.Main h="100%"
+        style={{paddingInlineStart: "var(--app-shell-padding)"}} //if this is not set mantine will recalculate padding when navbar is open, causing main content to change size
+      >
+        <Center h="inherit">
+          <Stack h="inherit">
+            <Routes>
+              <Route path="/home" element={<HomePage/>}/>
+              <Route path="/*" element={<HomePage/>}/>
+            </Routes>
+            <Text ta="center" mt="auto">
+              © of kfmx, 2024
+            </Text>
+          </Stack>
+        </Center>
       </AppShell.Main>
+
     </AppShell>
   )
 }
-
-export default App
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,91 @@
+import styles from './../Styles.module.css';
+import { Carousel } from "@mantine/carousel"
+import { Button, Paper, Text, Title } from '@mantine/core';
+import { Link } from "react-router-dom";
+import Autoplay from 'embla-carousel-autoplay';
+import { useRef } from 'react';
+
+interface CardProps {
+  image: string,
+  title: string,
+  titleDarkText: boolean,
+  pathname: string
+}
+
+const carouselCards: CardProps[] = [
+  {
+    image: "https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-1.png",
+    title: "Game Design",
+    titleDarkText: true,
+    pathname: "/gamedesign"
+  },
+  {
+    image: "https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-2.png",
+    title: "Project Leading",
+    titleDarkText: false,
+    pathname: "/projectleading"
+  },
+  {
+    image: "https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-3.png",
+    title: "Web Programming",
+    titleDarkText: true,
+    pathname: "/webprogramming"
+  },
+  {
+    image: "https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-5.png",
+    title: "Game Programming",
+    titleDarkText: false,
+    pathname: "/gameprogramming"
+  },
+];
+
+function Card({image, title, titleDarkText, pathname}: CardProps) {
+  return (
+    <Paper
+      shadow="md"
+      p="xl"
+      radius="md"
+      style={{ backgroundImage: `url(${image})` }}
+      className={styles.card}
+    >
+      <Title className={styles.cardTitle} c={titleDarkText ? "var(--mantine-color-black)" : "var(--mantine-color-white)"} pos="absolute">
+        {title}
+      </Title>
+      <Link className={styles.cardLink} to={pathname} tabIndex={-1}>
+        <Button>
+          Read more
+        </Button>
+      </Link>
+    </Paper>
+  );
+}
 
 export default function HomePage() {
-
+  const autoplay = useRef(Autoplay({ delay: 7000 }));
+  const slides = carouselCards.map(card => (
+      <Carousel.Slide key={card.title}>
+        <Card {...card} />
+      </Carousel.Slide>
+    ));
+  
     return (
-        <>
-            <h1>Welcome!</h1>
-            <p>
-                This is still a work in progress :)
-            </p>
-        </>
+      <>
+        <Text size="xl">
+          Hello and welcome to my portfolio! I have worked professionally as both a programmer and project lead for 4+ years on a variety of projects, ranging from web development to game design. I have studied game design at a bachelors level, and am currently working on a solo game project. Please use the carousel below or the navigation menu in the header to read more about my work.
+        </Text>
+        <Text>
+          Thank you very much for visiting! :)
+        </Text>
+        <Carousel
+          slideGap={{ base: 'xl', sm: 2 }}
+          loop
+          plugins={[autoplay.current]}
+          onMouseEnter={autoplay.current.stop}
+          onMouseLeave={autoplay.current.reset}
+          withIndicators
+        >
+          {slides}
+        </Carousel>
+      </>
     )
 }
